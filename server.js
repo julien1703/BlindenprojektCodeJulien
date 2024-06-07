fc2914543165239b33413112725417fd8d0b27f6
FR4NZP <kidpfisto@gmail.com>
2024-06-07 13:06:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -135,6 +135,8 @@ app.post('/speak', async (req, res) => {
     }
 });
 
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
